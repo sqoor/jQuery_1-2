2cb955585932c67a916e7ee4fce54fbe1e558794
sqoor <alsqoorAbdullah@gmail.com>
2019-07-23 09:34:32 +0300

    solution exercises 1 - 3

--- jQuery-1/jQuery.js
+++ jQuery-1/jQuery.js
@@ -3,18 +3,42 @@ Q1:
 Check if jQuery is loaded
 */
 
+$(document).ready(function() {
+    $('#q1').text('jQuery is working');
+});
+
 
 /*
 Q2:
 If You would have very long website - How to help user with fast getting back on top :)
 */
 
+$(document).ready(function () {
+    $('#goUpBtn').on('click', function () {
+        console.log('go up');
+        $("html, body").scrollTop(0);
+    });
+ });
+
 
 /*
 Q3:
 If You would like to change sizes of Your div/button with on click :)
 */
+$(document).ready(function() {
+    var changeBtn = $('#btn1');
+    var resetBtn = $('#btn2');
+    var box = $('#box');
+    var origianDimentions = [box.height(), box.width()];
+   
+    changeBtn.on('click', function() {
+        box.animate({height: '300px', width: '300px'})
+    });
 
+    resetBtn.on('click', function() {
+        box.animate({height: origianDimentions[0], width: origianDimentions[1]});
+    });
+});
 
 /*
 Q4:
